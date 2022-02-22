25ca2be33af67116a4b25c916be6ac6d4d8b8137
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-02-22 19:44:58 +0900

    Molecules//Organisms Plan の作成

---
+++ src/stories/molecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default { title: "Molecules/plan" };
+
+export const Example = () => Item_Example;
