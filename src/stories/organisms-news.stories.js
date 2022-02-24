24b916fb38b0d008923c5c22d0a8715219bb7fb3
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-02-24 13:13:50 +0900

    organisms Newsの作成

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "Organisms/news" };
+
+export const Example = () => Item_Example;
