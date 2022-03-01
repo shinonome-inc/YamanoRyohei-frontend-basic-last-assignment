104869936a60737a90cf7da81f04ad35df8162dd
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-03-01 21:44:02 +0900

    planのOrganismとの分離、タグの変更。organismの完成

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/news-and-events-more" };
+
+export const Example = () => Item_Example;
