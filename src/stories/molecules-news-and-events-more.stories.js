79e5a4ab3de3b72ea5313a86571059b3af26e34d
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-03-26 21:24:05 +0900

    試験的作業,差分削除

--- src/stories/molecules-news-and-events-more.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/molecules/news-and-events-more/news-and-events-more.html";
-import "../components/molecules/news-and-events-more/news-and-events-more.scss";
-
-export default { title: "Molecules/news-and-events-more" };
-
-export const Example = () => Item_Example;
--- src/stories/organisms-plan.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/organisms/plan/plan.html";
-import "../components/organisms/plan/plan.scss";
-
-export default { title: "Organisms/plan" };
-
-export const Example = () => Item_Example;
