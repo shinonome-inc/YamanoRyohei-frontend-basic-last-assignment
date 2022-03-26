9f4e6196812fc280f322100787346fb85315e62e
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-03-27 02:59:03 +0900

    PlanのPR 再作成

---
+++ src/stories/organisms-plan.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/organisms/plan/plan.html";
+import "../components/organisms/plan/plan.scss";
+
+export default { title: "Organisms/Plan" };
+
+export const Example = () => Item_Example;
