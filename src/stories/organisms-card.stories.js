245a855599f0a26e27dde2be93c3d32b1cd3ecca
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-03-26 19:53:06 +0900

    不要な差分を削除する、試験的作業

--- src/stories/organisms-card.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/organisms/card/card.html";
-import "../components/organisms/card/card.scss";
-
-export default { title: "Organisms/card" };
-
-export const Example = () => Item_Example;
