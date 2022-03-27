2a51e30f880d3b8f0b14e3e47132015eda9e5239
山野遼平 <yamanoryouhei@yamanoryouheinoMacBook-Air.local>
2022-03-27 19:24:15 +0900

    差分削除、したはず

--- src/stories/molecules-newsandeventstitle.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/molecules/news-and-events-title/news-and-events-title.html";
-import "../components/molecules/news-and-events-title/news-and-events-title.scss";
-
-export default { title: "Molecules/news-and-events-title" };
-
-export const Example = () => Item_Example;
