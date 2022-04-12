e3acfe29ae4c1f3603e271b5648919a8af50d0a6
Yanmannu <84486196+Yanmannu@users.noreply.github.com>
2022-04-13 01:13:27 +0900

    Revert "最終課題提出"

---
+++ src/stories/organisms-events.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/organisms/events/events.html";
+import "../components/organisms/events/events.scss";
+
+export default { title: "Organisms/events" };
+
+export const Example = () => Item_Example;
