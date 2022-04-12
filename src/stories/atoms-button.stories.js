02c9d871330daec24907cddf1304ef6dbd8ab496
Yanmannu <Yanmannu@users.noreply.github.com>
2022-04-12 15:37:43 +0000

    deploy: 6c087dc388b7ddfc1013d56de118f7036e54f33f

--- src/stories/atoms-button.stories.js
+++
@@ -1,4 +0,0 @@
-import Item_Example from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-export default { title: "Atoms/button" };
-export const Example = () => Item_Example;
--- src/stories/molecules-message.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_Example from "../components/molecules/message/message.html";
-import "../components/molecules/message/message.scss";
-
-export default { title: "Molecules/Message" };
-
-export const Example = () => Item_Example;
